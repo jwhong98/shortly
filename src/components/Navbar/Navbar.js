fc99dceb317b696ff2838204e52167eed6128920
jwhong <jwhong98@gmail.com>
2022-03-03 10:20:38 -0800

    added nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,26 @@
 import React from "react";
+import classes from "./Navbar.module.css";
+import logo from "../../images/logo.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className={classes.navContainer}>
+        <img src={logo} alt="logo" className={classes.logo} />
+        <div className={classes.navLinks}>
+          <ul className={classes.navMenu}>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Resources</li>
+          </ul>
+          <div className={classes.user}>
+            <a href="">Login</a>
+            <button className={classes.button}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
