8f3ce472272a82c9d1ee90b581a437911adb09b2
jwhong <jwhong98@gmail.com>
2022-03-23 11:47:08 -0700

    added link validation

--- src/components/URLInput/URLInput.js
+++ src/components/URLInput/URLInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LinkCard from "./LinkCard";
 import classes from "./URLInput.module.css";
 
@@ -6,28 +6,42 @@ const URLInput = (props) => {
   const [link, setLink] = useState("");
   const [shortened, setShortened] = useState([]);
 
+  const input = document.querySelector("#inputLink");
+  const errorMsg = document.querySelector("#error");
+
   // useEffect(() => {
   //   const list = localStorage.getItem('shortened');
 
   // })
 
+  const isUrl = (s) => {
+    var regexp =
+      /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(s);
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    if (link === "") {
-      alert("invalid");
+
+    if (!isUrl(link)) {
+      input.classList.add(classes["invalid"]);
+      errorMsg.style.display = "block";
     } else {
       const url = "https://api.shrtco.de/v2/shorten?url=" + link;
       fetch(url)
         .then((res) => res.json())
         .then((result) => {
-          console.log(shortened);
           let short = result.result.short_link;
           const shortObj = {
             id: Math.random().toString(),
             url: link,
             shrt: short,
           };
+          input.classList.remove(classes["invalid"]);
+          errorMsg.style.display = "none";
           setShortened((prevArr) => [...prevArr, shortObj]);
+          input.value = "";
+          console.log("complete");
         });
     }
   };
@@ -38,18 +52,24 @@ const URLInput = (props) => {
 
   return (
     <section className={classes.inputSection}>
-      <div className={classes.inputSectionContainer}>
-        <form action="" onSubmit={submitHandler}>
-          <input
-            type="text"
-            className={classes.formInput}
-            placeholder="Shorten a link here..."
-            onChange={(e) => setLink(e.target.value)}
-          />
-          <div className={classes.buttonContainer}>
-            <button type="submit">Shorten It!</button>
-          </div>
-        </form>
+      <div className={classes.inputWrap}>
+        <div className={classes.inputSectionContainer}>
+          <form action="" onSubmit={submitHandler}>
+            <input
+              type="text"
+              id="inputLink"
+              className={classes.formInput}
+              placeholder="Shorten a link here..."
+              onChange={(e) => setLink(e.target.value)}
+            />
+            <div className={classes.buttonContainer}>
+              <button type="submit">Shorten It!</button>
+            </div>
+          </form>
+          <p className={classes.invalidMessage} id="error">
+            Please add a link
+          </p>
+        </div>
       </div>
       <div className={classes.shortList}>{shortened.map(createLinkCard)}</div>
     </section>
